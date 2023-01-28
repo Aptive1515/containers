8574369f0db469dd7d698d6d7cb58e834a7cafa8
aptive <thibaut1515@hotmail.fr>
2023-01-28 17:48:32 +0100

    end pair/equal

--- includes/ft_utils.hpp
+++ includes/ft_utils.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_utils.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tdelauna <tdelauna@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aptive <aptive@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 13:57:53 by tdelauna          #+#    #+#             */
-/*   Updated: 2023/01/27 14:43:06 by tdelauna         ###   ########.fr       */
+/*   Updated: 2023/01/28 17:33:20 by aptive           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,10 @@ This is asked so you can discover SFINAE.
 
 namespace ft
 {
-	// ********************************** PAIR *******************************************
+	// +---------------------------------------------------------------------------------+
+	// |                                    PAIR                                         |
+	// +---------------------------------------------------------------------------------+
+
 	template <typename T1, typename T2>
 	class pair
 	{
@@ -54,27 +57,103 @@ namespace ft
 			pair (const pair<T1,T2>& pr) : first(pr.first), second(pr.second) {};
 		// Inialization
 			pair (const T1 & a, const T2 & b) : first(a), second(b) {};
-
 		// overload
-			// pair& operator=(const pair& pr){};
-
+			pair& operator=(const pair& pr){
+				this->first = pr.first;
+				this->second = pr.second;
+				return *this;
+			};
 		// Destructeur
 			~pair ( void ) {};
 
 		// Methode
-			// swap
-
 		public :
 			T1	first;
 			T2	second;
 	};
 	// Non-member function overloads ----------------------------------------------
 
-		// elational operators (pair)	Relational operators for pair (function template)
-		// swap (pair)	Exchanges the contents of two pairs (function template)
-		// get (pair)	Get element (tuple interface) (function template)
+	// elational operators (pair)	Relational operators for pair (function template)
+	template <class T1, class T2>
+		bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return lhs.first==rhs.first && lhs.second==rhs.second; }
+
+	template <class T1, class T2>
+		bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(lhs==rhs); }
+
+	template <class T1, class T2>
+		bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return lhs.first<rhs.first || (!(rhs.first<lhs.first) && lhs.second<rhs.second); }
+
+	template <class T1, class T2>
+		bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(rhs<lhs); }
+
+	template <class T1, class T2>
+		bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return rhs<lhs; }
+
+	template <class T1, class T2>
+		bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(lhs<rhs); }
+
+	// +---------------------------------------------------------------------------------+
+	// |                                  END PAIR                                       |
+	// +---------------------------------------------------------------------------------+
 
 
+
+	// +---------------------------------------------------------------------------------+
+	// |                                 MAKE PAIR                                       |
+	// +---------------------------------------------------------------------------------+
+	template <typename T1, typename T2>
+	pair<T1,T2> make_pair (T1 x, T2 y)
+	{
+		ft::pair<T1, T2> dest;
+
+		dest.first = x;
+		dest.second = y;
+
+		return dest;
+	}
+	// +---------------------------------------------------------------------------------+
+	// |                                   EQUAL                                         |
+	// +---------------------------------------------------------------------------------+
+		/*
+		* Compare les éléments de la plage [first1,last1)avec ceux de la plage
+		* commençant par first2 et renvoie true si tous
+		* les éléments des deux plages correspondent.
+		* Les éléments sont comparés à l'aide de operator==(ou pred , dans la version (2) ).
+		*/
+
+	template <class InputIterator1, class InputIterator2>
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+	{
+		while (first1!=last1)
+		{
+			if (!(*first1 == *first2))   // or: if (!pred(*first1,*first2)), for version 2
+				return false;
+			++first1; ++first2;
+		}
+		return true;
+	};
+
+	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
+	{
+		while (first1!=last1)
+		{
+			if (!pred(*first1,*first2))
+				return false;
+			++first1; ++first2;
+		}
+		return true;
+	};
+
+	// +---------------------------------------------------------------------------------+
+	// |                                 END EQUAL                                       |
+	// +---------------------------------------------------------------------------------+
 
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tdelauna <tdelauna@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aptive <aptive@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 18:06:51 by aptive            #+#    #+#             */
-/*   Updated: 2023/01/27 14:47:00 by tdelauna         ###   ########.fr       */
+/*   Updated: 2023/01/28 17:38:07 by aptive           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_utils.hpp"
+#include <vector>
+#include <algorithm>    // std::equal
 
 #include <utility>      // std::pair, std::make_pair
 #include <string>       // std::string
@@ -22,55 +24,49 @@
 # define END "\033[0m"
 
 
+bool mypredicate (int i, int j) {
+	return (i==j);
+}
+
 int main()
 {
 
-	std::cout << "----------------------------- TEST FT_UTILS --------------------------" << std::endl;
+	std::cout << "+----------------------------------------------------------------+" << std::endl;
+	std::cout << "|                        TEST FT_UTILS                           |" << std::endl;
+	std::cout << "+----------------------------------------------------------------+" << std::endl;
 
-	std::cout << "----------------------------- TEST PAIR --------------------------" << std::endl;
+	std::cout << "|************************** TEST PAIR ***************************|" << std::endl;
 
-
+	// Test Make_pair ---------------------------------------------------------------------
 		ft::pair <std::string,double> product1_ft;                     // default constructor
 		std::pair <std::string,double> product1;                     // default constructor
 
-
-		// product1 = std::make_pair(std::string("lightbulbs"),0.99);   // using make_pair (move)
 		product1 = std::make_pair(std::string("lightbulbs"),0.99);   // using make_pair (move)
-		// std::cout << "The price of " << product1.first << " is $" << product1.second << '\n';
-		// std::cout << "The price of " << product3.first << " is $" << product3.second << '\n';
+		product1_ft = ft::make_pair(std::string("lightbulbs"),0.99);   // using make_pair (move)
 
-		// std::cout << "The price of " << product1.first << " is $" << product1.second << '\n';
+		if (product1.first == product1_ft.first && product1.second == product1_ft.second)
+			std::cout << GREEN << "	Test Make_pair :				OK" << END << std::endl;
+		else
+			std::cout << RED << "	Test Make_pair :				Not OK" << END << std::endl;
 
-
-
+	// Test initialisation ----------------------------------------------------------------
 		ft::pair <std::string,double> product2_ft ("tomatoes",2.30);   // value init
 		std::pair <std::string,double> product2 ("tomatoes",2.30);   // value init
 
-
-
-
-
-
-
-
 		if (product2_ft.first == product2.first && product2_ft.second == product2.second)
-			std::cout << GREEN << "Test initialisation : OK" << END << std::endl;
+			std::cout << GREEN << "	Test initialisation :				OK" << END << std::endl;
 		else
-			std::cout << RED << "Test initialisation : Not OK" << END << std::endl;
+			std::cout << RED << "	Test initialisation :				Not OK" << END << std::endl;
 
-
-
-
-
+	// Test copy --------------------------------------------------------------------------
 		ft::pair <std::string,double> product3_ft (product2_ft);          // copy constructor
 		std::pair <std::string,double> product3 (product2);          // copy constructor
 
-		// product2.first = "shoes";                  // the type of first is string
-
 		if (product3_ft.first == product3.first && product3_ft.second == product3.second)
-			std::cout << GREEN << "Test copy : OK" << END << std::endl;
+			std::cout << GREEN << "	Test copy :					OK" << END << std::endl;
 		else
-			std::cout << RED << "Test copy : Not OK" << END << std::endl;
+			std::cout << RED << "	Test copy :					Not OK" << END << std::endl;
+	// Test operator = attribut -----------------------------------------------------------
 
 		product3.first = "shoes";                  // the type of first is string
 		product3_ft.first = "shoes";                  // the type of first is string
@@ -78,11 +74,42 @@ int main()
 		product3_ft.second = 39.90;                   // the type of second is double
 
 		if (product3_ft.first == product3.first && product3_ft.second == product3.second)
-			std::cout << GREEN << "Test operator = direct attribut : OK" << END << std::endl;
+			std::cout << GREEN << "	Test operator = direct attribut :		OK" << END << std::endl;
 		else
-			std::cout << RED << "Test operator = direct attribut : Not OK" << END << std::endl;
+			std::cout << RED << "	Test operator = direct attribut :		Not OK" << END << std::endl;
 
-		std::cout << "----------------------------- END TEST PAIR --------------------------" << std::endl;
+	// Test operator = attribut -----------------------------------------------------------
+		ft::pair <std::string,double> product4_ft = product2_ft;          // copy constructor
+		if (product2_ft.first == product4_ft.first && product2_ft.second == product4_ft.second)
+			std::cout << GREEN << "	Test operator = attribut :			OK" << END << std::endl;
+		else
+			std::cout << RED << "	Test operator = attribut :			Not OK" << END << std::endl;
+
+	std::cout << "|********************** END TEST PAIR ***************************|" << std::endl;
+	std::cout << std::endl << std::endl;
+	std::cout << "|************************** TEST EQUAL **************************|" << std::endl;
+	std::cout << "|********************** END TEST EQUAL **************************|" << std::endl;
+
+	int myints[] = {20,40,60,80,100};               //   myints: 20 40 60 80 100
+	std::vector<int>myvector (myints,myints+5);     // myvector: 20 40 60 80 100
+
+	// using default comparison:
+	if ( ft::equal (myvector.begin(), myvector.end(), myints) )
+		std::cout << "The contents of both sequences are equal.\n";
+	else
+		std::cout << "The contents of both sequences differ.\n";
+
+	myvector[3]=81;                                 // myvector: 20 40 60 81 100
+
+	// using predicate comparison:
+	if ( ft::equal (myvector.begin(), myvector.end(), myints, mypredicate) )
+		std::cout << "The contents of both sequences are equal.\n";
+	else
+		std::cout << "The contents of both sequences differ.\n";
+
+	std::cout << "+----------------------------------------------------------------+" << std::endl;
+	std::cout << "|                        END FT_UTILS                            |" << std::endl;
+	std::cout << "+----------------------------------------------------------------+" << std::endl;
 
 
 
